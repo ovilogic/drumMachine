2fab28ff49aab7feea1f8672e2ce31eade132a26
ovilogic <ovidiu.opreah@yahoo.com>
2022-03-28 12:35:39 +0100

    added <audio>, referenced it with ref and added useRef hook to make it play

--- src/components/pad.jsx
+++ src/components/pad.jsx
@@ -1,41 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 import '../style/pad.css'
 
 
 const Pad = () => {
 
- 
-        const q = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3')
-        const w = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3')
-        const e = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3')
-        const a  = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3')
-        const s  = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3')
-        const d  = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3')
-        const z  = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3')
-        const x  = new Audio('https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3')
-        const c  = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3')
-       
-        
-
-        
-
-        
-        
-
+    const q = useRef()
 
     return ( 
         <div className='board'>
-            <div className='drum-pad' onClick={() => {q.play()}}>Q</div>
+            <div className='drum-pad' onClick={() => {q.current.play()}}> Q
+                <audio ref={q} >
+                    <source  className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+                    type='audio/mp3'>
+                    </source>
+
+                </audio>
+                </div>
               
-            <div className='drum-pad' onClick={() => w.play()}>W</div>
-                
-            <div className='drum-pad' onClick={() => e.play()}>E</div>
-            <div className='drum-pad' onClick={() => a.play()}>A</div>
-            <div className='drum-pad' onClick={() => s.play()}>S</div>
-            <div className='drum-pad' onClick={() => d.play()}>D</div>
-            <div className='drum-pad' onClick={() => z.play()}>Z</div>
-            <div className='drum-pad' onClick={() => x.play()}>X</div>
-            <div className='drum-pad' onClick={() => c.play()}>C</div>
+            <div className='drum-pad' >W</div>                
+            <div className='drum-pad' >E</div>
+            <div className='drum-pad' >A</div>
+            <div className='drum-pad' >S</div>
+            <div className='drum-pad' >D</div>
+            <div className='drum-pad' >Z</div>
+            <div className='drum-pad' >X</div>
+            <div className='drum-pad' >C</div>
 
         </div>
      );
