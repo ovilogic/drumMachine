2119b7620f6bb6757fbc135f4b8298d5bfb4cc57
ovilogic <ovidiu.opreah@yahoo.com>
2022-04-02 10:28:33 +0100

    no errors, but state is not updated satisfactorily in the document event listener. Possible cause: the lack of an eventListener remover.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,95 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Pad from "./components/pad";
 import './style/app.css'
 
 
 
 function App() {
- const [on, setOn] = useState(true)
 
- const handlePower = () => {
-   setOn(!on)
- }
- console.log(on)
+  const q = useRef()
+  const w = useRef()
+  const e = useRef()
+  const a = useRef()
+  const s = useRef()
+  const d = useRef()
+  const z = useRef()
+  const x = useRef()
+  const c = useRef()
+
+  const keys = [
+    {
+        press: 'Q',
+        ref: q,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    },
+    {
+        press: 'W',
+        ref: w,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+        press: 'E',
+        ref: e,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    {
+        press: 'A',
+        ref: a,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    {
+        press: 'S',
+        ref: s,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+        press: 'D',
+        ref: d,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+        press: 'Z',
+        ref: z,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+        press: 'X',
+        ref: x,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+        press: 'C',
+        ref: c,
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    } 
+    ]
+
+
+
+const [on, setOn] = useState(true)
+
+const handlePower = () => {
+  setOn(!on)
+}
+
+const handlePlay = (ref) => {
+  if (on == true) {
+      console.log('in handlePlay   ', ref.current)
+      ref.current.play()}        
+}
+console.log(on)
+
+
+useEffect(() => {
+  document.addEventListener('keypress', (event) => {
+    let key = keys.filter(k => event.key.toUpperCase() == k.press );
+    console.log(key[0], 'in doc event listener')
+    console.log('in eventLIstener  ', on)
+    
+    handlePlay(key[0].ref)
+  })
+  
+}, [on])
 
 
   return (
@@ -18,7 +97,7 @@ function App() {
     <div id='drum-machine'>
       
       <div id='display'>
-        <Pad power={on} handlePower={handlePower} />
+        <Pad handlePower={handlePower} handlePlay={handlePlay} keys={keys} />
       </div>
       
     </div>
--- src/components/pad.jsx
+++ src/components/pad.jsx
@@ -3,111 +3,18 @@ import React, { useState, useEffect, useRef } from 'react';
 import '../style/pad.css'
 
 
-const Pad = ({ power, handlePower }) => {
+const Pad = ({ keys, handlePower, handlePlay }) => {
 
-    const q = useRef()
-    const w = useRef()
-    const e = useRef()
-    const a = useRef()
-    const s = useRef()
-    const d = useRef()
-    const z = useRef()
-    const x = useRef()
-    const c = useRef()
-
-    const keys = [
-        {
-            press: 'Q',
-            ref: q,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-        },
-        {
-            press: 'W',
-            ref: w,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-        },
-        {
-            press: 'E',
-            ref: e,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-        },
-        {
-            press: 'A',
-            ref: a,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-        },
-        {
-            press: 'S',
-            ref: s,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-        },
-        {
-            press: 'D',
-            ref: d,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-        },
-        {
-            press: 'Z',
-            ref: z,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-        },
-        {
-            press: 'X',
-            ref: x,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-        },
-        {
-            press: 'C',
-            ref: c,
-            src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-        } 
-        ]
-
-
-    const handlePlay = (ref) => {
-        if (power == true) {
-            ref.current.play()}        
-    }
-            
-       
-    let keysArr = [];
-    for (let i = 0; i < keys.length; i++) {
-        let x = keys[i].press
-        keysArr.push(x.toUpperCase())
-    }   
-    
-    document.addEventListener('keypress', (event) => {
-        
-        var name = event.key;
-     
-        
-        for (let i = 0; i < keysArr.length; i++) {
-            
-            if (name.toUpperCase() == keysArr[i]) {
-                let keysObj = keys.map(item => {
-                    if (item.press == name.toUpperCase()) {
-                        handlePlay(item.ref)
-                        
-                    }
-                }
-                )
-            }
-    }
-    }, false);
-
-    
-        
-    
+  
 
     let keyboard = () => keys.map(item => 
-        <div key={item.press} id={item.press} className='drum-pad' onClick={() => handlePlay(item.ref)}> 
+        <div key={item.press} id={item.press} className='drum-pad'
+         onClick={() => handlePlay(item.ref)}> 
             <h1>{item.press}</h1>
             <audio id={item.press} ref={item.ref} className='clip' src={item.src} ></audio>
         </div>
     
     )
-
-   
 
     return (
         <div className='motherboard'> 
